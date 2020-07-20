b12380e04d18efb9e15d236d67a377d7bc309806
TanyaChichkan <TanyaChichkan@users.noreply.github.com>
2020-07-21 04:49:50 +0800

    new

--- task-6.js
+++ task-6.js
@@ -1,81 +1,71 @@
 
 
-let input;
-const numbers = [];
-let total = 0;
+// let input;
+// const numbers = [];
+// let total = 0;
 
-do {
-    input = prompt('Insert a number');
+// do {
+//     input = prompt('Insert a number');
 
 
-    const newInput = Number(input);
-    if (Number.isNaN(newInput)) {
-        alert('A type different from number was inserted. Try again');
-        continue;
+//     const newInput = Number(input);
+//     if (Number.isNaN(newInput)) {
+//         alert('A type different from number was inserted. Try again');
+//         continue;
+//     }
+
+//     numbers.push(newInput);
+
+
+// } while (input !== null);
+
+// numbers.pop();
+// console.log(numbers);
+
+
+// for (let element of numbers) {
+//     total += element;
+
+// } console.log(`Total is ${total}`);
+
+
+
+
+let numbers = [];
+const insertUserInputRef = document.querySelector('input[name="userInput"]');
+
+const buttonRef = document.querySelector('button');
+
+buttonRef.addEventListener('click', function () {
+
+    let total = 0;
+    const input = Number(insertUserInputRef.value);
+    // console.log(input);
+    insertUserInputRef.value = '';
+    numbers.push(input);
+    console.log(numbers);
+
+    for (let number of numbers) {
+        total += number;
     }
 
-    numbers.push(newInput);
+
+    console.log(total);
+
+    // if (input == '') {
+    //     numbers.pop();
+    //     alert('Try again');
+    // }
 
 
-} while (input !== null);
-
-numbers.pop();
-console.log(numbers);
-
-
-for (let element of numbers) {
-    total += element;
-
-} console.log(`Total is ${total}`);
+    // if (Number.isNaN(input)) {
+    //     numbers.pop();
+    //     alert('Not a number');
+    // }
 
 
+});
 
-// let total = 0;
-// let numbers = [];
-// const insertUserInputRef = document.querySelector('input[name="userInput"]');
-
-// const buttonRef = document.querySelector('button');
-
-// buttonRef.addEventListener('click', function () {
-
-
-//     const input = Number(insertUserInputRef.value);
-//     console.log(input);
-//     insertUserInputRef.value = '';
-//     numbers.push(input);
-//     console.log(numbers);
-
-
-
-//     // if (input == '') {
-//     //     // numbers.pop();
-//     //     alert('Try again');
-//     //     continue;
-
-//     // }
-
-//     if (Number.isNaN(input)) {
-//         numbers.pop();
-//         alert('Not a number');
-//     }
-
-
-
-
-
-
-//     for (let i = 0; i < numbers.length; i += 1) {
-//         // console.log(element);
-//         total += numbers[i];
-
-//     }
-//     // console.log(numbers);
-
-//     console.log(`Total is ${total}`);
-
-
-
-// });
 
 
 
